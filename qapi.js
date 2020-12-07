515f69861f8d11f044e45fdeaf450de2ca70b309
Ishit Sharma <75165904+IshitSharma@users.noreply.github.com>
2020-12-07 12:59:54 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcdd79098cea1001eadcb3f",
+				"5fcdd79098cea1001eadcb41"
+			]
+		},
+		"5fcdd79098cea1001eadcb3f": {
+			"id": "5fcdd79098cea1001eadcb3f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcdd79098cea1001eadcb41": {
+			"id": "5fcdd79098cea1001eadcb41",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://img.utdstc.com/screen/1/android-studio-005.jpg)">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-primary" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--dark"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0" color="#ffffff">
+					Hey I’m Androider
+				</Text>
+				<Text font="--base" color="#ffffff">
+					I am a cool guy
+				</Text>
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
